55b09b9fd5d5df79bebbd4464826020a4d8c15e6
aaronzh <aaron@burnsred.com.au>
2019-02-23 12:23:11 +1100

    add more directions and more speeds for aliens

--- alien.py
+++ alien.py
@@ -1,6 +1,5 @@
 import pygame
 from pygame.sprite import Sprite
-from random import randint
 from random import random
 
 
@@ -22,6 +21,22 @@ class Alien(Sprite):
         self.y = float(self.rect.y)
         self.direction_flag = True
         self.change_direction_flag = True
+        if random() > 0.2:
+            self.straight_flag = False
+        else:
+            self.straight_flag = True
+        if random() > 0.95:
+            self.alien_speed_factor_x = self.settings.alien_speed_factor_x * 3
+            self.alien_speed_factor_y = self.settings.alien_speed_factor_y * 1.2
+        elif random() > 0.9:
+            self.alien_speed_factor_x = self.settings.alien_speed_factor_x * 2
+            self.alien_speed_factor_y = self.settings.alien_speed_factor_y * 1.2
+        elif random() > 0.75:
+            self.alien_speed_factor_x = self.settings.alien_speed_factor_x * 1.5
+            self.alien_speed_factor_y = self.settings.alien_speed_factor_y * 1.1
+        else:
+            self.alien_speed_factor_x = self.settings.alien_speed_factor_x
+            self.alien_speed_factor_y = self.settings.alien_speed_factor_y
 
     def blit_me(self):
         """Draw the alien at its current location."""
@@ -29,28 +44,29 @@ class Alien(Sprite):
 
     def update(self):
         """Move the alien."""
-        if self.rect.right >= self.screen.get_rect().right:
-            self.x -= self.settings.alien_speed_factor_x
-            self.direction_flag = False
-        elif self.rect.left <= 0:
-            self.x += self.settings.alien_speed_factor_x
-            self.direction_flag = True
-        elif self.change_direction_flag:
-            if random() - 0.5 > 0:
-                self.x += self.settings.alien_speed_factor_x
-                self.direction_flag = True
-            else:
-                self.x -= self.settings.alien_speed_factor_x
+
+        if not self.straight_flag:
+            if self.rect.right >= self.screen.get_rect().right:
+                self.x -= self.alien_speed_factor_x
                 self.direction_flag = False
-        elif not self.change_direction_flag:
-            if self.direction_flag:
-                self.x += self.settings.alien_speed_factor_x
-            else:
-                self.x -= self.settings.alien_speed_factor_x
+            elif self.rect.left <= 0:
+                self.x += self.alien_speed_factor_x
+                self.direction_flag = True
+            elif self.change_direction_flag:
+                if random() > 0.5:
+                    self.x += self.alien_speed_factor_x
+                    self.direction_flag = True
+                else:
+                    self.x -= self.alien_speed_factor_x
+                    self.direction_flag = False
+            elif not self.change_direction_flag:
+                if self.direction_flag:
+                    self.x += self.alien_speed_factor_x
+                else:
+                    self.x -= self.alien_speed_factor_x
         self.change_direction_flag = False
 
-
-        self.y += self.settings.alien_speed_factor_y
+        self.y += self.alien_speed_factor_y
 
         self.rect.x = self.x
         self.rect.y = self.y
